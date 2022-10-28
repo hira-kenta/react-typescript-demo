b61de349fa2940394bf3ce476fd4c7617096ed1c
Kenta Hirakata <hira.ken.0512.14@gmail.com>
2022-10-28 14:16:50 +0900

    React props学習

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
+  const personName = {
+    first: "Kenta",
+    last: "Hirakata"
+  }
+
+  const nameList = [
+    {
+      first: "Hirakata",
+      last: "Kenta"
+    },
+    {
+      first: "Hirakata",
+      last: "Misato"
+    },
+    {
+      first: "Horie",
+      last: "Popo"
+    },
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Status status="success" />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+      <Greet name='Kenta' isLoggedIn={true}></Greet>
     </div>
   );
 }
